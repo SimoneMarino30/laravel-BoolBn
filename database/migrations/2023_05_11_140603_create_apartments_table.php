62d178a56e678b2551d86a91dca72cd0c2bf3c88
Simone Marino <simo.marino@outlook.it>
2023-05-11 16:38:42 +0200

    Apartment migration

---
+++ database/migrations/2023_05_11_140603_create_apartments_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            
+            $table->foreignId('user_id')
+            ->constrained()
+            ->cascadeOnDelete();
+
+            $table->string('title', 100);
+            $table->text('description');
+            $table->unsignedTinyInteger('rooms');
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('bathrooms');
+            $table->unsignedSmallInteger('mq');
+            $table->text('address');
+            $table->string('latitude', 100);
+            $table->string('longitude', 100);
+            $table->float('price', 6,2 )->unsigned();
+            $table->string('image', 255)->nullable();
+            $table->string('slug', 255)->unique();
+            $table->boolean('visibility')->default(1);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
