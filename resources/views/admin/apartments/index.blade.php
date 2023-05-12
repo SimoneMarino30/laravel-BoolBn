7093b7dc9d5c934b2bd7bd54958756bd4b34b8d2
Simone Marino <simo.marino@outlook.it>
2023-05-12 19:36:21 +0200

    View index + Navbar

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,1 +1,24 @@
-@dump($apartments);
+{{-- @dump($apartments); --}}
+
+@extends('layouts.app')
+
+@section('page-name', 'Appartamenti')
+
+@section('content')
+  <section class="container">
+    <h1 class="text-center">Lista Appartamenti</h1>
+    <div class="row">
+      @forelse ($apartments as $apartment)
+      <div class="col-4">
+        <div class="card">
+          <div class="card-header">
+            {{ $apartment-> title }}
+          </div>
+        </div>
+      </div>
+      @empty
+         🤦‍♂️   
+      @endforelse
+    </div>
+  </section>
+@endsection
