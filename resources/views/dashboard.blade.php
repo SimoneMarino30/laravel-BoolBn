47198dbb029d8107da5b2cee92d802ff7d6b0a45
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-13 13:18:15 +0200

    modify style navbar + route link

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
-                <div class="card-header">{{ __('User Dashboard') }}</div>
+                <div class="card-header">{{ __('Dashboard utente') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,7 +17,7 @@
                     </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Pagina statistiche. Work in progress...') }}
                 </div>
             </div>
         </div>
