ac8b264bd1125254ac033887038998ed7b310272
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-17 10:26:50 +0200

    improve message alert

--- resources/views/layouts/partials/_session-message.blade.php
+++ resources/views/layouts/partials/_session-message.blade.php
@@ -1,5 +1,6 @@
 @if (session('message_content'))
-    <div class="alert alert-{{session('message_type') ? session('message_type') : 'success'}}">
+    <div class="alert alert-{{session('message_type') ? session('message_type') : 'success'}} d-flex justify-content-between">
         {{session('message_content')}}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 @endif
