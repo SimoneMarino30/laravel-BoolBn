2de5f76977a10a56fb916face7d5db7fd9bbe945
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-17 15:14:20 +0200

    fix login page-name + navbar user logged

--- resources/views/layouts/partials/_navbar.blade.php
+++ resources/views/layouts/partials/_navbar.blade.php
@@ -49,7 +49,7 @@
         "></i>
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
             aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }}
+            {{ Auth::user()->email }}
           </a>
           
               <div class="dropdown-menu dropdown-menu-right dropdown-menu-dark">
