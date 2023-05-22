fc97c8b3be01b9f05bfe41c410caa6ef1f3600e6
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-22 17:17:14 +0200

    add index view

---
+++ resources/views/admin/sponsors/index.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('page-name', 'Sponsor')
+
+@section('content')
+
+    <section id="sponsors_list" class="container py-5">
+        <h1 class="mt-5">Sponsorizzazioni</h1>
+
+        <div class="row">
+            @forelse($sponsors as $sponsor)
+                <div class="col-12 col-md-4">
+                    <div class="my-card">
+                        <div
+                            class="my-card-header d-flex align-items-center justify-content-between p-3 @if ($sponsor->type == 'Silver') silver @elseif ($sponsor->type == 'Gold') gold @elseif ($sponsor->type == 'Platinum') platinum @endif">
+                            <h4>
+                                {{ $sponsor->type }}</h4>
+
+                            <span class="sponsor-price fs-3">
+                                € {{ $sponsor->price }}
+                            </span>
+                        </div>
+                        <div class="my-card-body">
+                            <p>
+                                {{ $sponsor->description }}
+                            </p>
+                        </div>
+                        <div class="my-card-footer text-end">
+                            <p class="text-muted p-2">
+                                Sponsorizza per una durata di {{ $sponsor->duration }} ore
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            @empty
+                <h2>Non ci sono sponsor</h2>
+            @endforelse
+        </div>
+    </section>
+
+@endsection
