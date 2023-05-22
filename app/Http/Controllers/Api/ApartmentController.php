1e060905c98563a0fe066364b47f38e7bc6c0cd8
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-22 15:34:38 +0200

    test filtered API Apartment

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -14,6 +14,7 @@ class ApartmentController extends Controller
      */
     public function index()
     {
+        // ! NO FILTRI
         $apartments = Apartment::where('visibility', true)->with('services')->orderBy('updated_at', 'DESC')->paginate(8);
 
         foreach($apartments as $apartment) {
@@ -21,6 +22,60 @@ class ApartmentController extends Controller
         }
 
         return response()->json($apartments);
+
+
+        // ! RICERCA PER INDIRIZZO
+        // if (request()->input('address')) {
+
+        //     $address = request()->input('address');
+
+        //     $apartments = Apartment::where('address', 'like', '%'.$address.'%')->where('visibility', true)->paginate(8);
+        // } else {
+        //     $apartments = Apartment::all();
+        // }
+
+        // $response = [
+        //     'success' => true,
+        //     'code' => 200,
+        //     'message' => 'La lista arriva',
+        //     'apartments' => $apartments
+        // ];
+        
+        // return response()->json($response);
+
+
+        // ! LOGICA COORDINATE E RAGGIO
+        // funzione distanza raggio
+        // function distance($lat1, $lon1, $lat2, $lon2)
+        // {
+        //     if (($lat1 == $lat2) && ($lon1 == $lon2)) {
+        //         return 0;
+        //     } else {
+        //         $theta = $lon1 - $lon2;
+        //         $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
+        //         $dist = acos($dist);
+        //         $dist = rad2deg($dist);
+        //         $kilometers = $dist * 60 * 1.1515 * 1.609344;
+        //         return $kilometers;
+        //     }
+        // }
+
+        // $lat = n;
+        // $lon = n;
+        // $range = 20;
+
+        // if (
+        //     request()->input('lat') &&
+        //     request()->input('lon')
+        // ) {
+        //     $lat = request()->input('lat');
+        //     $lon = request()->input('lon');
+        // }
+        // if (request()->input('range'))
+        //     $range = request()->input('range');
+
+        // ORDINAMENTO RISPOSTA PER DISTANZA
+        // "(6371 * acos(cos(radians(" . $lat . ")) * cos(radians(latitude)) * cos(radians(longitude) - radians(" . $lon . ")) + sin(radians(" . $lat . ")) * sin(radians(latitude)))) asc";
     }
 
     /**
