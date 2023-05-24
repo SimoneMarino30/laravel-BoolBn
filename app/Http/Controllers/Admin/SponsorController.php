f0b88523bf30ffcd9831b8ab56d375831d7c5caa
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-05-24 19:38:17 +0200

    add SponsorController show

--- app/Http/Controllers/Admin/SponsorController.php
+++ app/Http/Controllers/Admin/SponsorController.php
@@ -6,6 +6,10 @@ use App\Http\Controllers\Controller;
 
 // MODELS
 use App\Models\Sponsor;
+use App\Models\Apartment;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class SponsorController extends Controller
@@ -51,10 +55,39 @@ class SponsorController extends Controller
      * @param  \App\Models\Sponsor  $sponsor
      * @return \Illuminate\Http\Response
      */
-    public function show(Sponsor $sponsor)
+    public function show(Sponsor $sponsor, Request $request)
     {
-        //
+        $apartment_id = $request->query('apartment_id');
+        
+        $user = Auth::user();
+        
+        $today = Carbon::today();
+
+        if ($apartment_id == null) {
+
+            $apartments = Apartment::where('user_id', $user->id)
+            ->whereDoesntHave('sponsors', function ($query)  use ($today) {
+                $query->where('expiring_date', '>', $today)
+                ->orWhereNull('expiring_date');
+            })->get();
+
+            return view('admin.sponsors.show', compact('apartments', 'sponsor'));
+        }
+        
+        else {
+
+            $apartment = Apartment::findOrFail($apartment_id);
+
+            $sponsor = Sponsor::findOrFail($sponsor->id);
+            
+            // Controllo apartment_id nell'url
+            if($user->id === $apartment->user_id){
+                return view('admin.sponsors.show', compact('apartment', 'sponsor'));
+             }   abort(403, "accesso non autorizzato");
+ 
+        }  
     }
+    
 
     /**
      * Show the form for editing the specified resource.
